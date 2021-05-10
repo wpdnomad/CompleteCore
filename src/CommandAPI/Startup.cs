be16b61f24a85cdb5762f0ba98082b110a765dfa
Bill D <wpdnomad@yahoo.com>
2021-05-10 15:48:02 -0700

    Switched from mock to sql repo in ConfigureServices

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -26,7 +26,7 @@ namespace CommandAPI
             services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
             (Configuration.GetConnectionString("PostgreSqlConnection")));
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
