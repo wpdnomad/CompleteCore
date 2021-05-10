952506c20542872b51f604c94e179de6bb9e473c
Bill D <wpdnomad@yahoo.com>
2021-05-10 15:41:07 -0700

    Add Postgres DB respository implementation in Data folder

---
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class SqlCommandAPIRepo : ICommandAPIRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
