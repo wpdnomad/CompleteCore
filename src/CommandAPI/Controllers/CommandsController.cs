fa9061606d4bf9c8cbd24708212b0961e943a764
Bill D <wpdnomad@yahoo.com>
2021-05-10 09:53:32 -0700

    Inject repo in CommandsController

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,10 +1,19 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+
 namespace CommandAPI.Controllers{
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase
     {
+
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repository)
+            {
+                _repository = repository;
+            }
+
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
