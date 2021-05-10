f9b37322465844c5367da5ef115555882743d6f0
Bill D <wpdnomad@yahoo.com>
2021-05-10 12:08:14 -0700

    Add GetCommandById to search repo for specific command

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,23 +1,39 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using CommandAPI.Data;
+using CommandAPI.Models;
 
-namespace CommandAPI.Controllers{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CommandsController : ControllerBase
-    {
-
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
         private readonly ICommandAPIRepo _repository;
-        public CommandsController(ICommandAPIRepo repository)
-            {
-                _repository = repository;
-            }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] {"this", "is", "hard", "coded"};
-        }
-    }
+        public CommandsController(ICommandAPIRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        //GET api/commands/{id}
+        //[Authorize]       //Apply this attribute to lockdown this ActionResult (or others)
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
+        }
+    }
 }
