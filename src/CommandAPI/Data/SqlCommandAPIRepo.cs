8ccb04a965c737dc504328c25baad9a97a1c2a1f
Bill D <wpdnomad@yahoo.com>
2021-05-11 13:23:49 -0700

    Added implementations for creating commands and the  SaveChanges method in sql repo

--- src/CommandAPI/Data/SqlCommandAPIRepo.cs
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -1,19 +1,26 @@
 using System.Collections.Generic;
 using CommandAPI.Models;
 using System.Linq;
+using System;
 
 namespace CommandAPI.Data
 {
     public class SqlCommandAPIRepo : ICommandAPIRepo
     {
         private readonly CommandContext _context;
-        public SqlCommandAPIRepo(CommandContext context)
-        {
-            _context = context;
-        }
+
+        public SqlCommandAPIRepo(CommandContext context)
+        {
+            _context = context;
+        }
+
         public void CreateCommand(Command cmd)
         {
-            throw new System.NotImplementedException();
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+            _context.CommandItems.Add(cmd);
         }
 
         public void DeleteCommand(Command cmd)
@@ -25,6 +32,7 @@ namespace CommandAPI.Data
         {
             return _context.CommandItems.ToList();
         }
+
         public Command GetCommandById(int id)
         {
             return _context.CommandItems.FirstOrDefault(p => p.Id == id);
@@ -32,7 +40,7 @@ namespace CommandAPI.Data
 
         public bool SaveChanges()
         {
-            throw new System.NotImplementedException();
+            return (_context.SaveChanges() >= 0);
         }
 
         public void UpdateCommand(Command cmd)
