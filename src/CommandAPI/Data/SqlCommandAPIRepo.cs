d23511379d462038d0202117e7896c1ec1b05aa1
Bill D <wpdnomad@yahoo.com>
2021-05-12 14:25:38 -0700

    Implement DeleteCommand in SQL repo

--- src/CommandAPI/Data/SqlCommandAPIRepo.cs
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -25,7 +25,11 @@ namespace CommandAPI.Data
 
         public void DeleteCommand(Command cmd)
         {
-            throw new System.NotImplementedException();
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+            _context.CommandItems.Remove(cmd);
         }
 
         public IEnumerable<Command> GetAllCommands()
