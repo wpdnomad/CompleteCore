55776afa3cfc79367a3ede72550e8b3b548e1798
Bill D <wpdnomad@yahoo.com>
2021-05-13 11:39:15 -0700

    Refactor common elements of controller tests to contructor

--- tests/CommandAPI.Tests/CommandsControllerTests.cs
+++ tests/CommandAPI.Tests/CommandsControllerTests.cs
@@ -5,23 +5,41 @@ using CommandAPI.Models;
 using CommandAPI.Profiles;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
 namespace CommandAPI.Tests
 {
-    public class CommandsControllerTests
+    public class CommandsControllerTests : IDisposable
     {
+        private Mock<ICommandAPIRepo> mockRepo;
+        private CommandsProfile realProfile;
+        private MapperConfiguration configuration;
+        private IMapper mapper;
+
+        public CommandsControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+
+        public void Dispose()
+        {
+            mockRepo = null;
+            mapper = null;
+            configuration = null;
+            realProfile = null;
+        }
+
         [Fact]
         public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty()
         {
             //Arrange
-            var mockRepo = new Mock<ICommandAPIRepo>();
             mockRepo.Setup(repo =>
               repo.GetAllCommands()).Returns(GetCommands(0));
-            var realProfile = new CommandsProfile();
-            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
-            IMapper mapper = new Mapper(configuration);
             var controller = new CommandsController(mockRepo.Object, mapper);
             //Act
             var result = controller.GetAllCommands();
