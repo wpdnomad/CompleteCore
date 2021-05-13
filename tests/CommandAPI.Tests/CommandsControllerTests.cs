1900620a450b2d2689bc851d9bedbadd1099ef6a
Bill D <wpdnomad@yahoo.com>
2021-05-13 11:24:04 -0700

    Add CommandsController tests with test for returning zero items when repo is empty

--- tests/CommandAPI.Tests/CommandsControllerTests.cs
+++ tests/CommandAPI.Tests/CommandsControllerTests.cs
@@ -1,4 +1,11 @@
 ﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using Moq;
+using AutoMapper;
+using CommandAPI.Models;
+using CommandAPI.Data;
+using CommandAPI.Controllers;
 
 namespace CommandAPI.Tests
 {
@@ -7,9 +14,27 @@ namespace CommandAPI.Tests
         [Fact]
         public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty()
         {
-            //Arrange
-            //We need to create an instance of our CommandsController class
-            var controller = new CommandsController( /* repository, AutoMapper */);
+            var mockRepo = new Mock<ICommandAPIRepo>();
+            mockRepo.Setup(repo =>
+              repo.GetAllCommands()).Returns(GetCommands(0));
+            var controller = new CommandsController(mockRepo.Object, /* AutoMapper*/ );
+        }
+
+        private List<Command> GetCommands(int num)
+        {
+            var commands = new List<Command>();
+            if (num > 0)
+            {
+                commands.Add(new Command
+                {
+                    Id = 0,
+                    HowTo = "How to generate a migration",
+                    CommandLine = "dotnet ef migrations add <Name of Migration>",
+                    Platform = ".Net Core EF"
+                });
+            }
+            return commands;
         }
     }
+}
 }
