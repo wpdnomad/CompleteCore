f876a50a3bf719b99a994e88c24be95f2f816d4a
Bill D <wpdnomad@yahoo.com>
2021-05-13 11:00:17 -0700

    Install Moq and Automapper in test project

---
+++ tests/CommandAPI.Tests/CommandsControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace CommandAPI.Tests
+{
+    public class CommandsControllerTests
+    {
+        [Fact]
+        public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty()
+        {
+            //Arrange
+            //We need to create an instance of our CommandsController class
+            var controller = new CommandsController( /* repository, AutoMapper */);
+        }
+    }
+}
